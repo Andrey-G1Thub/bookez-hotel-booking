be5f701e9e38c162b60b94c84b6ee3010bcb51de
ITman87 <rukshenasandrii@gmail.com>
2026-02-02 15:31:09 +0300

    Реализация не известной страници

---
+++ src/pages/notFoundPage/NotFoundPage.tsx
@@ -0,0 +1,12 @@
+export const NotFoundPage = ({ message = 'Маршрут не найден.', navigate }) => (
+	<div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-20 text-center">
+		<h2 className="text-6xl font-extrabold accent-text mb-4">404</h2>
+		<p className="text-2xl text-gray-700 mb-6">{message}</p>
+		<button
+			onClick={() => navigate('/')}
+			className="mt-6 inline-block py-3 px-8 rounded-lg text-white font-semibold accent-color accent-hover transition shadow-lg"
+		>
+			<Home className="w-5 h-5 inline mr-2 -mt-1" /> Вернуться на Главную
+		</button>
+	</div>
+);
