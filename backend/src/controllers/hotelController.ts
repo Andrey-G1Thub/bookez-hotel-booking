b94333ecc2a3c444bee6b22708c05c314454706f
ITman87 <rukshenasandrii@gmail.com>
2026-02-25 21:54:03 +0300

    Реализация удаления отеля и номера отеля

--- backend/src/controllers/hotelController.ts
+++ backend/src/controllers/hotelController.ts
@@ -1,6 +1,5 @@
 import { Request, Response } from 'express'
 import { Hotel } from '../models/Hotel'
-// import { Hotel } from '../models/Hotel'
 
 export const getHotels = async (req: Request, res: Response) => {
   try {
@@ -41,5 +40,19 @@ export const updateHotel = async (req: Request, res: Response) => {
   } catch (error) {
     console.error('Update Error:', error)
     res.status(400).json({ message: 'Ошибка при обновлении', error })
+  }
+}
+export const deleteHotel = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params
+    const deletedHotel = await Hotel.findByIdAndDelete(id)
+
+    if (!deletedHotel) {
+      return res.status(404).json({ message: 'Отель не найден' })
+    }
+
+    res.json({ message: 'Отель успешно удален' })
+  } catch (error) {
+    res.status(500).json({ message: 'Ошибка при удалении отеля', error })
   }
 }
--- backend/src/roures/hotelRoutes.ts
+++ backend/src/roures/hotelRoutes.ts
@@ -3,6 +3,7 @@ import {
   getHotels,
   createHotel,
   updateHotel,
+  deleteHotel,
 } from '../controllers/hotelController'
 
 const router = Router()
@@ -10,5 +11,6 @@ const router = Router()
 router.get('/', getHotels) // GET http://localhost:5000/api/hotels
 router.post('/', createHotel) // POST http://localhost:5000/api/hotels
 router.patch('/:id', updateHotel)
+router.delete('/:id', deleteHotel)
 
 export default router
