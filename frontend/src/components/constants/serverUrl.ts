f59e00b5afa0f8ea1ff1d81ca7987295b0da9992
ITman87 <rukshenasandrii@gmail.com>
2026-03-10 13:28:01 +0300

    исправление адреса для фото

--- frontend/src/components/constants/serverUrl.ts
+++ frontend/src/components/constants/serverUrl.ts
@@ -1,1 +1,2 @@
-export const SERVER_URL = 'http://localhost:5000';
+// export const SERVER_URL = 'http://localhost:5000';
+export const SERVER_URL = import.meta.env.DEV ? 'http://localhost:5000' : '';
